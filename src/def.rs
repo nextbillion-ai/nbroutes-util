c50813f6b79add32213ae9762a7ca836dcbed8af
zixin wang <zixin@nextbillion.ai>
2022-03-29 09:58:37 +0000

    optimization input avoid as None

--- src/def.rs
+++ src/def.rs
@@ -55,7 +55,7 @@ pub struct OptimizationInput {
     pub geometries: Option<String>,
     #[doc = "apikey for authentication.\n\nDefault: `\"\"`"]
     pub key: Option<String>,
-    pub avoid: Option<String>,
+    pub avoid: None,
 }
 
 #[derive(Serialize, Deserialize, Apiv2Schema)]
