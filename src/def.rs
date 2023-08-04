a5d1b7cc829e77a7ce3fe5ba0a1c331db55f3901
Youzhi Yang <youzhi@nextbillion.ai>
2023-08-04 10:17:41 +0800

    feat:add travelled_raw_locations

--- src/def.rs
+++ src/def.rs
@@ -619,6 +619,8 @@ pub struct NavigatingInput {
     pub route_type: Option<String>,
     #[doc = "road info objects to include in response.\n\nFormat: `type1|type2,...`.\n\nDefault:`\"\"`"]
     pub road_info: Option<String>,
+    #[doc = "location(s) of waypoint(s) along the trip.\n\nFormat: `lat0,lng0|lat1,lng1|...`.\n\nRegex: (^[\\d\\.\\-]+,[\\d\\.\\-]+(\\|[\\d\\.\\-]+,[\\d\\.\\-]+)*$)"]
+    pub travelled_raw_locations: Option<String>,
 }
 
 #[derive(Serialize, Deserialize, Apiv2Schema)]
